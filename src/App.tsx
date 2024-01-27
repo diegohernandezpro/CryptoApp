b2dec2e9ccd39be272bea592c7482cdc9154b2de
Diego Hernandez <diegohc2003@gmail.com>
2024-01-27 16:14:52 -0500

    installed tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>ThIS IS MASTER Branch</h1>
+      <h1 className="bg-amber-200 text-red-400">ThIS is Tailwind</h1>
+      <p className="bg-blue-200 text-right text-orange-800"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Porro libero hic earum, in sit, voluptatem ducimus, tenetur dolor id tempore illo. Similique, beatae? Hic iure eligendi inventore numquam, voluptates modi!</p>
     </>
   )
 }
