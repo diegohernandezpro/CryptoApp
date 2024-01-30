37abcae28a9fdf4c9a0f968b599ca4fbe9b40a29
Diego Hernandez <diegohc2003@gmail.com>
2024-01-29 19:26:49 -0500

    added icons to infographic

--- src/components/MainHeader/Header/Header.tsx
+++ src/components/MainHeader/Header/Header.tsx
@@ -13,11 +13,27 @@ export function Header(){
                     <span className="text-[#D1D1D1]">Coins</span><span>7884</span>
                 
                 </li>
-                <li className="header-item"><span className="text-[#D1D1D1]">Exchanges</span><span>662</span></li>
+                <li className="header-item">
+                <img
+                    src={"/src/assets/exchange-icon.svg"}
+                    alt='exchange-icon'
+                /> 
+                <span className="text-[#D1D1D1]">Exchanges</span><span>662</span></li>
                 <li className="header-item">1.66T</li>
                 <li className="header-item">$124.900B</li>
-                <li className="header-item">44%</li>
-                <li className="header-item">21%</li>
+                <li className="header-item">
+                <img
+                    src={"/src/assets/bitcoin.svg"}
+                    alt='bitcoin-icon'
+                /> 
+                <span>44%</span></li>
+                <li className="header-item">
+
+                <img
+                    src={"/src/assets/etherum.svg"}
+                    alt='etherum-icon'
+                /><span>21%</span>
+                </li>
             </ul>
         </header>
     )
