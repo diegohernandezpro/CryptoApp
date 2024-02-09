13c957cfb2f193a8bab1a89507c6599ed4723e02
Diego Hernandez <diegohc2003@gmail.com>
2024-02-09 14:24:26 -0500

    icon for currency and theme

--- src/components/Header/CurrencySelector/currencySelector.tsx
+++ src/components/Header/CurrencySelector/currencySelector.tsx
@@ -6,16 +6,28 @@ import React from "react";
 export default function CurrencySelector() {
   const dispatch = useDispatch<AppDispatch>();
   const currency = useSelector((state: RootState) => state.currency);
+  const isDark = useSelector((state: RootState) => state.theme.isDark);
 
   return (
-    <div className="flex gap-2 py-3 px-4 justify-around items-center w-[108px] bg-sec-blue-blacksih border border-theme-dark-border rounded-md overflow-auto text-theme-dark cursor-pointer">
+    <div
+      className={[
+        "canvas-item flex gap-2 py-3 px-4 justify-around items-center w-[108px]",
+        `theme-${isDark ? "galaxy" : "glacier"}`,
+      ]
+        .filter(Boolean)
+        .join(" ")}
+    >
       <img
-        src="src/assets/currency-selector.svg"
+        src={
+          isDark
+            ? "src/assets/currency-icon-galaxy.svg"
+            : "src/assets/currency-icon-glacier.svg"
+        }
         alt="currency-selector-icon"
-        className="w-5 h-5"
+        className="w-6 h-6"
       />
       <select
-        className="bg-sec-blue-blacksih font-mono appearance-none cursor-pointer"
+        className="font-mono appearance-none cursor-pointer bg-skin-base text-header-base"
         value={currency.currency}
         onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
           dispatch(setCurrency(e.target.value))
