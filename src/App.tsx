d7ae9def79bc007e09b56bf00a1bab3f040c814a
Diego Hernandez <diegohc2003@gmail.com>
2024-02-16 11:01:15 -0500

    added theme from root removed individual themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "./state/store";
 import Infographic from "@/components/Infographic";
 import Header from "@/components/Header";
 
 export default function App() {
+  const isDark = useSelector((state: RootState) => state.theme.isDark);
+
+  useEffect(() => {
+    console.log("reloading app based on theme change isDark: ", isDark);
+    document.documentElement.classList.toggle("dark", isDark);
+  }, [isDark]);
+
   return (
-    <div className="h-screen app-css">
+    <div className={`app-css`}>
       <Infographic />
       <Header />
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -37,7 +37,6 @@ export default {
     },
     colors: {},
   },
-
   plugins: [],
 };
 
