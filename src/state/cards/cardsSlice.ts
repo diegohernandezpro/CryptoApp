041317d799379cfbe86575d39a8818c334f3d68e
Diego Hernandez <diegohc2003@gmail.com>
2024-02-20 13:23:39 -0500

    cards on redux

--- src/state/cards/cardsSlice.ts
+++ src/state/cards/cardsSlice.ts
@@ -24,20 +24,36 @@ const cardsSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder;
-    // .addCase(getData.pending, (state) => {
-    //   state.status = FETCHING_STATE.PENDING;
-    // })
-    // .addCase(getData.fulfilled, (state, action: PayloadAction<Data>) => {
-    //   state.status = FETCHING_STATE.FULFILLED;
-    //   state.coinsData = action.payload;
-    // })
-    // .addCase(getData.rejected, (state) => {
-    //   state.status = FETCHING_STATE.REJECTED;
-    // });
+    builder
+      .addCase(getData.pending, (state) => {
+        state.status = FETCHING_STATE.PENDING;
+      })
+      .addCase(getData.fulfilled, (state, action: PayloadAction<Data>) => {
+        state.status = FETCHING_STATE.FULFILLED;
+        state.coinsData = action.payload;
+      })
+      .addCase(getData.rejected, (state) => {
+        state.status = FETCHING_STATE.REJECTED;
+      });
   },
 });
 
-export const getData = createAsyncThunk("graphCards/getData", async () => {});
+export const getData = createAsyncThunk(
+  "graphCards/getData",
+  async (_, { getState }) => {
+    const { currency } = (getState() as RootState).currency;
+    const currencyType = currency.toLocaleLowerCase();
+
+    const response = await api(
+      "/coins/markets",
+      `?vs_currency=${currencyType}&order=market_cap_desc&per_page=50&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d`
+    );
+    console.log("🚀 ~ getData ~ response", response);
+
+    const jsonresponse = JSON.stringify(response);
+    const jsonData = JSON.parse(jsonresponse);
+    console.log("🚀 ~ jsonData:", jsonData);
+  }
+);
 
 export default cardsSlice.reducer;
